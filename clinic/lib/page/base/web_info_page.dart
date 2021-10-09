1f91435004fd178b39f0c2eecd3204f81f365da9
dungnguyen2703 <dungnguyen2703@live.com>
2021-10-09 16:13:36 +0700

    add webview

--- clinic/lib/page/base/web_info_page.dart
+++ clinic/lib/page/base/web_info_page.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:webviewx/webviewx.dart';
 
 class WebInfoPage extends StatelessWidget {
   String get title => "";
@@ -8,12 +11,22 @@ class WebInfoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Container(
-        child: Text(url),
+      body: WebViewX(
+        initialContent: 'Loading',
+        initialSourceType: SourceType.html,
+        height: screenSize.height / 2,
+        width: min(screenSize.width * 0.8, 1024),
+        onWebViewCreated: (controller) {
+          controller.loadContent(
+            url,
+            SourceType.url,
+          );
+        },
       ),
     );
   }
